80f3635de166d705556ac5c682baf2e31c1761ea
dhruvesh2907 <mehtadhruvesh2907@gmail.com>
2025-03-03 10:00:22 +0530

    populate

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -11,5 +11,6 @@ routes.get("/user",userController.getAllUsers)
 routes.post("/user",userController.adduser)
 routes.delete("/user/:id",userController.deleteUser)
 routes.get("/user/:id",userController.getUserById)
+routes.post("/user/login",userController.login)
 
 module.exports = routes 
